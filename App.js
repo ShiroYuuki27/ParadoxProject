5725d4313d973b55f7c786ad2c5b5b9bd4e36914
ShiroYuuki27 <adityaekacool@gmail.com>
2023-10-16 21:05:06 +0700

    kidil jangan mencuri

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Home';
-import Login from './src/Login';
-import SignUp from './src/SignUp';
-const Stack = createNativeStackNavigator();
-    function App() {
-        return (
-            <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-            </Stack.Navigator>
-            </NavigationContainer>
-        );
+import { Text, View, Switch } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App1() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  return (
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Switch value={colorScheme === 'dark'} onChange={toggleColorScheme} />
+      <Text className="dark:text-white text-2xl font-bold">New collection</Text>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+    </View>
+  );
 }
-export default App; 
